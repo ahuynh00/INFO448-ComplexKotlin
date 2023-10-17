0874eb94518969cbf9992151985ca783e75faedd
AHUYNH00 <AHUYNH00@UW.EDU>
2023-10-17 02:45:52 -0700

    tests pass

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,8 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.71"
+    //id("org.jetbrains.kotlin.jvm") version "1.3.71"
+    id("org.jetbrains.kotlin.jvm") version "1.6.0"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
--- src/main/kotlin/edu/uw/complexkotlin/Library.kt
+++ src/main/kotlin/edu/uw/complexkotlin/Library.kt
@@ -9,7 +9,21 @@ package edu.uw.complexkotlin
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ for 0..15.
 // store this lambda into 'fizzbuzz' so that the tests can call it
 //
-val fizzbuzz : (IntRange) -> String = { _ -> "" }
+val fizzbuzz : (IntRange) -> String = {
+    range -> range.map({
+        if ((it % 3 == 0) && (it % 5 == 0)) {
+            "FIZZBUZZ"
+        } else if (it % 3 == 0) {
+            "FIZZ"
+        } else if (it % 5 == 0) {
+            "BUZZ"
+        // } else if (it % 7 == 0) {
+        //     "DOH!"
+        } else {
+            ""
+        }
+    }).fold("", {string, next -> string + next})
+}
 
 // Example usage
 /*
@@ -33,25 +47,45 @@ fun Int.times(block: () -> Unit): Unit {
 fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
-// Create r1 as a lambda that calls process() with message "FOO" 
+// Create r1 as a lambda that calls process() with message "FOO"
 // and a block that returns "BAR"
-val r1 = { "" }
+val r1 = { process("FOO", {"BAR"})}
 
-// Create r2 as a lambda that calls process() with message "FOO" 
-// and a block that upper-cases r2_message, and repeats it three 
+// Create r2 as a lambda that calls process() with message "FOO"
+// and a block that upper-cases r2_message, and repeats it three
 // times with no spaces: "WOOGAWOOGAWOOGA"
 val r2_message = "wooga"
-val r2 = { "" }
+val r2 = {process("FOO", {
+    var res = r2_message
+    2.times({res += r2_message})
+    res.uppercase()
+})}
+
 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher {
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString() = "Deep thoughts...." //extra . to match test
+    },
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString()= "Allow me to suggest an idea..."
+    };
 
-// create an class "Command" that can be used as a function 
+    abstract fun signal(): Philosopher
+}
+
+// create an class "Command" that can be used as a function
 // (provide an "invoke()" function)
 // that takes a single parameter ("message" of type String)
 // primary constructor should take a String argument ("prompt")
 // when invoked, the Command object should return a String c
 // ontaining the prompt and then the message.
 // Example: Command(": ")("Hello!") should print ": Hello!"
-class Command(val prompt: String) { }
+class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return "" + prompt + message
+    }
+ }
